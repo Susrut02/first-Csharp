41ddc9abb9c7b2307bc4dd1d3afa6e7dc51320a5
sus rut <104177720+Susrut02@users.noreply.github.com>
2023-04-15 13:42:55 -0700

    Add files via upload

---
+++ susrut1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hello World!");
+Console.Write("ayo sus");
+Console.WriteLine("amogus");
+int susNum = 5;
+Console.WriteLine(susNum * 20);
+
+Console.WriteLine("Enter username:");
+string username = Console.ReadLine();
+Console.WriteLine("Enter password:");
+string password = Console.ReadLine();
+Console.WriteLine("username: " + username);
+Console.WriteLine("password: " + password);
+
+Console.WriteLine("Fahrenheit to Celcius");
+Console.WriteLine("Enter Fahrenheit value:");
+int temp = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((temp - 32) * 5 / 9 + " degrees Celcius");
